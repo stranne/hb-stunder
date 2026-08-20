2b4d1c8064a28df26897da4cb744a4e6fb6be76d
Olof Stranne <github.com.epic386@passmail.com>
2026-08-20 09:35:06 +0200

    fix(ui): keep open account icon visible

--- src/app/AppMenu.stories.tsx
+++ src/app/AppMenu.stories.tsx
@@ -37,6 +37,7 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const SignedIn: Story = {
+  globals: { colorMode: "light" },
   play: async ({ canvasElement }) => {
     await userEvent.click(within(canvasElement).getByRole("button"));
   },
